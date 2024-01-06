3d66ec4f7eec4efbcd671e37d68d11c47ef5baf8
Janek Szynal <jan.szynal@gmail.com>
2024-01-06 20:11:18 +0100

    Add download script and example index.html

---
+++ download-and-cleanup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+BASE_URL="https://untested.webflow.io"
+
+# Pull and prettify index.html
+wget -nv -O "index.html" $BASE_URL
+prettier -w *.html
+
+# Deletions for webflow branding and fluff
+# sed -i "" -e "/data-wf/d" index.html
+# sed -i "" -e "/<meta content=\"Webflow\"/d" index.html
+# sed -i "" -e "/\/js\/webflow/d" index.html
+
+# prettier -w index.html
